8e94f770f089d5597a827b06b8101fc4eda1922a
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2023-02-24 11:39:10 +0300

    State handled with handleChange function

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -1,12 +1,39 @@
-import react from 'react'
-import reactDOM from 'react-dom/client'
+import react, { useState } from "react";
+import reactDOM from "react-dom/client";
+import { Form, Button, Container } from "react-bootstrap";
 
 const Signup = () => {
-    return (
-        <div>
-        <h1>Signup</h1>
-        </div>
-    )
-    }
+  const [data, setData] = useState({});
+
+  const handleChanage = (event) => {
+    setData({ ...data, ...{ [event.target.name]: event.target.value } });
+  };
+
+  return (
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            name="email"
+            placeholder="Enter email"
+            onChange={(event) => handleChanage(event)}
+          />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="email"
+            name="password"
+            placeholder="Enter Password"
+            onChange={(event) => handleChanage(event)}
+          />
+        </Form.Group>
+      </Form>
+  );
+};
 
 export default Signup;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Signup from "./components/signup";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
 
 const Main = () => {
   return (
-    <div>
-      <Signup />
-    </div>
+    <Container className="d-flex justify-content-center align-items-center">
+      <div className="w-100 bg-primary" style={{ maxWidth: "400px" }}>
+        <Signup />
+      </div>
+    </Container>
   );
 };
 
